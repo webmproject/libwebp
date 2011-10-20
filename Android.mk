d8329d41e370757c0f4dd30708b30c0b8699215a
James Zern <jzern@google.com>
2011-10-19 20:02:23 -0700

    Android.mk: add missing source files
    Fell out of date with some of the dsp.c moves.
    Fixes Issue #93.
    
    Change-Id: I2fb4424dbb0bc31af236cef2bed5ee542622067b

--- Android.mk
+++ Android.mk
@@ -3,26 +3,26 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
 	src/dec/alpha.c \
-	src/dec/dsp.c \
+	src/dec/buffer.c \
 	src/dec/frame.c \
 	src/dec/idec.c \
+	src/dec/io.c \
 	src/dec/layer.c \
 	src/dec/quant.c \
 	src/dec/tree.c \
 	src/dec/vp8.c \
 	src/dec/webp.c \
-	src/dec/io.c \
-	src/dec/buffer.c \
-	src/dsp/yuv.c \
-	src/dsp/upsampling.c \
 	src/dsp/cpu.c \
 	src/dsp/dec.c \
 	src/dsp/dec_neon.c \
 	src/dsp/enc.c \
+	src/dsp/upsampling.c \
+	src/dsp/yuv.c \
 	src/enc/alpha.c \
 	src/enc/analysis.c \
 	src/enc/config.c \
-	src/enc/dsp.c \
+	src/enc/cost.c \
+	src/enc/enc.c \
 	src/enc/filter.c \
 	src/enc/frame.c \
 	src/enc/iterator.c \
@@ -31,7 +31,7 @@ LOCAL_SRC_FILES := \
 	src/enc/quant.c \
 	src/enc/syntax.c \
 	src/enc/tree.c \
-	src/enc/webpenc.c
+	src/enc/webpenc.c \
 	src/utils/bit_reader.c \
 	src/utils/bit_writer.c \
 	src/utils/thread.c \
