webmproject/libwebp

commit 252028aaacd4342bfe225b3dcb7e3be823f55e66

Add predictive filtering option for Alpha.
Add predictive filtering option for Alpha plane.
Valid range for filter option is [0, 5] corresponding to prediction
methods none, horizontal, vertical, gradient & paeth filter.
The prediction method 5 will try all the prediction methods (0 to 4)
and pick the prediction method that gives best compression.

Change-Id: I9244d4a9c5017501a9696c7cec5045f04c16d49b

src/utils/filters.c
@@ -1,0 +1,267 @@
+// Copyright 2011 Google Inc.
+//
+// This code is licensed under the same terms as WebM:
+//  Software License Agreement:  http://www.webmproject.org/license/software/
+//  Additional IP Rights Grant:  http://www.webmproject.org/license/additional/
+// -----------------------------------------------------------------------------
+//
+// Spatial prediction using various filters
+//
+// Author: Urvang (urvang@google.com)
+
+#include "./filters.h"
+#include <assert.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+//------------------------------------------------------------------------------
+// Helpful macro.
+
+# define SANITY_CHECK(in, out)                              \
+  assert(in != NULL);                                       \
+  assert(out != NULL);                                      \
+  assert(width > 0);                                        \
+  assert(height > 0);                                       \
+  assert(bpp > 0);                                          \
+  assert(stride >= width * bpp);
+
+//------------------------------------------------------------------------------
+// Horizontal filter.
+
+static void HorizontalFilter(const uint8_t* data, int width, int height,
+                             int bpp, int stride, uint8_t* filtered_data) {
+  int h;
+  SANITY_CHECK(data, filtered_data);
+
+  // Filter line-by-line.
+  for (h = 0; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = filtered_data + h * stride;
+
+    memcpy((void*)out, (const void*)scan_line, bpp);
+    for (w = bpp; w < width * bpp; ++w) {
+      out[w] = scan_line[w] - scan_line[w - bpp];
+    }
+  }
+}
+
+static void HorizontalUnfilter(const uint8_t* data, int width, int height,
+                               int bpp, int stride, uint8_t* recon_data) {
+  int h;
+  SANITY_CHECK(data, recon_data);
+
+  // Unfilter line-by-line.
+  for (h = 0; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = recon_data + h * stride;
+
+    memcpy((void*)out, (const void*)scan_line, bpp);
+    for (w = bpp; w < width * bpp; ++w) {
+      out[w] = scan_line[w] + out[w - bpp];
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// Vertical filter.
+
+static void VerticalFilter(const uint8_t* data, int width, int height,
+                           int bpp, int stride, uint8_t* filtered_data) {
+  int h;
+  SANITY_CHECK(data, filtered_data);
+
+  // Copy top scan-line as it is.
+  memcpy((void*)filtered_data, (const void*)data, width * bpp);
+
+  // Filter line-by-line.
+  for (h = 1; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = filtered_data + h * stride;
+    const uint8_t* const prev_line = scan_line - stride;
+    for (w = 0; w < width * bpp; ++w) {
+      out[w] = scan_line[w] - prev_line[w];
+    }
+  }
+}
+
+static void VerticalUnfilter(const uint8_t* data, int width, int height,
+                             int bpp, int stride, uint8_t* recon_data) {
+  int h;
+  SANITY_CHECK(data, recon_data);
+
+  // Copy top scan-line as it is.
+  memcpy((void*)recon_data, (const void*)data, width * bpp);
+
+  // Unfilter line-by-line.
+  for (h = 1; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = recon_data + h * stride;
+    const uint8_t* const out_prev_line = out - stride;
+    for (w = 0; w < width * bpp; ++w) {
+      out[w] = scan_line[w] + out_prev_line[w];
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// Gradient filter.
+
+static void GradientFilter(const uint8_t* data, int width, int height,
+                           int bpp, int stride, uint8_t* filtered_data) {
+  int h;
+  SANITY_CHECK(data, filtered_data);
+
+  // Copy top scan-line as it is.
+  memcpy((void*)filtered_data, (const void*)data, width * bpp);
+
+  // Filter line-by-line.
+  for (h = 1; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = filtered_data + h * stride;
+    const uint8_t* const prev_line = scan_line - stride;
+    memcpy((void*)out, (const void*)scan_line, bpp);
+    for (w = bpp; w < width * bpp; ++w) {
+      const uint8_t predictor = scan_line[w - bpp] + prev_line[w] -
+                                prev_line[w - bpp];
+      out[w] = scan_line[w] - predictor;
+    }
+  }
+}
+
+static void GradientUnfilter(const uint8_t* data, int width, int height,
+                             int bpp, int stride, uint8_t* recon_data) {
+  int h;
+  SANITY_CHECK(data, recon_data);
+
+  // Copy top scan-line as it is.
+  memcpy((void*)recon_data, (const void*)data, width * bpp);
+
+  // Unfilter line-by-line.
+  for (h = 1; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = recon_data + h * stride;
+    const uint8_t* const out_prev_line = out - stride;
+    memcpy((void*)out, (const void*)scan_line, bpp);
+    for (w = bpp; w < width * bpp; ++w) {
+      const uint8_t predictor = out[w - bpp] + out_prev_line[w] -
+                                out_prev_line[w - bpp];
+      out[w] = scan_line[w] + predictor;
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+// Paeth filter.
+
+static inline int AbsDiff(int a, int b) {
+  return (a > b) ? a - b : b - a;
+}
+
+static inline uint8_t PaethPredictor(uint8_t a, uint8_t b, uint8_t c) {
+  const int p = a + b - c;  // Base.
+  const int pa = AbsDiff(p, a);
+  const int pb = AbsDiff(p, b);
+  const int pc = AbsDiff(p, c);
+
+  // Return nearest to base of a, b, c.
+  return (pa <= pb && pa <= pc) ? a : (pb <= pc) ? b : c;
+}
+
+static void PaethFilter(const uint8_t* data, int width, int height,
+                        int bpp, int stride, uint8_t* filtered_data) {
+  int w;
+  int h;
+  SANITY_CHECK(data, filtered_data);
+
+  // Top scan line (special case).
+  memcpy((void*)filtered_data, (const void*)data, bpp);
+  for (w = bpp; w < width * bpp; ++w) {
+    // Note: PaethPredictor(scan_line[w - bpp], 0, 0) == scan_line[w - bpp].
+    filtered_data[w] = data[w] - data[w - bpp];
+  }
+
+  // Filter line-by-line.
+  for (h = 1; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = filtered_data + h * stride;
+    const uint8_t* const prev_line = scan_line - stride;
+    for (w = 0; w < bpp; ++w) {
+      // Note: PaethPredictor(0, prev_line[w], 0) == prev_line[w].
+      out[w] = scan_line[w] - prev_line[w];
+    }
+    for (w = bpp; w < width * bpp; ++w) {
+      out[w] = scan_line[w] - PaethPredictor(scan_line[w - bpp], prev_line[w],
+                                             prev_line[w - bpp]);
+    }
+  }
+}
+
+static void PaethUnfilter(const uint8_t* data, int width, int height,
+                          int bpp, int stride, uint8_t* recon_data) {
+  int w;
+  int h;
+  SANITY_CHECK(data, recon_data);
+
+  // Top scan line (special case).
+  memcpy((void*)recon_data, (const void*)data, bpp);
+  for (w = bpp; w < width * bpp; ++w) {
+    // Note: PaethPredictor(out[w - bpp], 0, 0) == out[w - bpp].
+    recon_data[w] = data[w] + recon_data[w - bpp];
+  }
+
+  // Unfilter line-by-line.
+  for (h = 1; h < height; ++h) {
+    int w;
+    const uint8_t* const scan_line = data + h * stride;
+    uint8_t* const out = recon_data + h * stride;
+    const uint8_t* const out_prev = out - stride;
+    for (w = 0; w < bpp; ++w) {
+      // Note: PaethPredictor(0, out_prev[w], 0) == out_prev[w].
+      out[w] = scan_line[w] + out_prev[w];
+    }
+    for (w = bpp; w < width * bpp; ++w) {
+      out[w] = scan_line[w] + PaethPredictor(out[w - bpp], out_prev[w],
+                                             out_prev[w - bpp]);
+    }
+  }
+}
+
+#undef SANITY_CHECK
+
+//------------------------------------------------------------------------------
+
+const WebPFilterFunc WebPFilters[WEBP_FILTER_LAST] = {
+    NULL,              // WEBP_FILTER_NONE
+    HorizontalFilter,  // WEBP_FILTER_HORIZONTAL
+    VerticalFilter,    // WEBP_FILTER_VERTICAL
+    GradientFilter,    // WEBP_FILTER_GRADIENT
+    PaethFilter,       // WEBP_FILTER_PAETH
+    NULL               // WEBP_FILTER_BEST
+};
+
+const WebPFilterFunc WebPUnfilters[WEBP_FILTER_LAST] = {
+    NULL,                // WEBP_FILTER_NONE
+    HorizontalUnfilter,  // WEBP_FILTER_HORIZONTAL
+    VerticalUnfilter,    // WEBP_FILTER_VERTICAL
+    GradientUnfilter,    // WEBP_FILTER_GRADIENT
+    PaethUnfilter,       // WEBP_FILTER_PAETH
+    NULL                 // WEBP_FILTER_BEST
+};
+
+//------------------------------------------------------------------------------
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}    // extern "C"
+#endif

Android.mk
@@ -34,6 +34,7 @@ LOCAL_SRC_FILES := \
 	src/utils/alpha.c \
 	src/utils/bit_reader.c \
 	src/utils/bit_writer.c \
+	src/utils/filters.c \
 	src/utils/quant_levels.c \
 	src/utils/tcoder.c \
 	src/utils/thread.c \

Makefile.vc
@@ -178,6 +178,7 @@ X_OBJS= \
     $(DIROBJ)\utils\alpha.obj \
     $(DIROBJ)\utils\bit_reader.obj \
     $(DIROBJ)\utils\bit_writer.obj \
+    $(DIROBJ)\utils\filters.obj \
     $(DIROBJ)\utils\quant_levels.obj \
     $(DIROBJ)\utils\tcoder.obj \
     $(DIROBJ)\utils\thread.obj \

examples/cwebp.c
@@ -700,6 +700,7 @@ static void HelpLong(void) {
   printf("  -map <int> ............. print map of extra info.\n");
   printf("  -d <file.pgm> .......... dump the compressed output (PGM file).\n");
   printf("  -alpha_method <int> .... Transparency-compression method (0..1)\n");
+  printf("  -alpha_filter <int> .... predictive filtering for Alpha (0..5)\n");
   printf("  -noalpha ............... discard any transparency information.\n");
 
   printf("\n");
@@ -793,6 +794,8 @@ int main(int argc, const char *argv[]) {
       config.alpha_quality = strtol(argv[++c], NULL, 0);
     } else if (!strcmp(argv[c], "-alpha_method") && c < argc - 1) {
       config.alpha_compression = strtol(argv[++c], NULL, 0);
+    } else if (!strcmp(argv[c], "-alpha_filter") && c < argc - 1) {
+      config.alpha_filtering = strtol(argv[++c], NULL, 0);
     } else if (!strcmp(argv[c], "-noalpha")) {
       keep_alpha = 0;
     } else if (!strcmp(argv[c], "-size") && c < argc - 1) {

makefile.unix
@@ -71,7 +71,8 @@ DSP_OBJS = src/dsp/cpu.o src/dsp/enc.o \
            src/dsp/dec_neon.o src/dsp/upsampling.o src/dsp/upsampling_sse2.o \
            src/dsp/yuv.o
 UTILS_OBJS = src/utils/alpha.o src/utils/bit_reader.o src/utils/bit_writer.o \
-             src/utils/quant_levels.o src/utils/thread.o src/utils/tcoder.o
+             src/utils/filters.o src/utils/quant_levels.o src/utils/thread.o \
+             src/utils/tcoder.o
 
 OBJS = $(DEC_OBJS) $(ENC_OBJS) $(DSP_OBJS) $(UTILS_OBJS)
 
@@ -81,7 +82,7 @@ HDRS = src/webp/encode.h src/enc/vp8enci.h src/enc/cost.h src/webp/mux.h \
        src/dec/vp8i.h  \
        src/dsp/yuv.h src/dsp/dsp.h \
        src/utils/alpha.h src/utils/bit_writer.h src/utils/bit_reader.h \
-       src/utils/thread.h src/utils/tcoder.h
+       src/utils/filters.h src/utils/thread.h src/utils/tcoder.h
 
 OUT_LIBS = src/libwebp.a src/mux/libwebpmux.a
 OUT_EXAMPLES = examples/cwebp examples/dwebp examples/webpmux

man/cwebp.1
@@ -41,6 +41,12 @@ Specify the compression factor for alpha compression between 0 and 100.
 Lossless compression of alpha is achieved using a value of 100, while the lower
 values result in a lossy compression. The default is 100.
 .TP
+.B \-alpha_filter int
+Specify the predictive filtering method (between 0 and 5) for alpha plane.
+These correspond to prediction modes none, horizontal, vertical, gradient and
+paeth filters. The prediction mode 5 will try all the prediction modes (0 to 4)
+and pick the best prediction mode. The default value is 0 (no prediction).
+.TP
 .B \-f int
 Specify the strength of the deblocking filter, between 0 (no filtering)
 and 100 (maximum filtering). A value of 0 will turn off any filtering.

src/enc/alpha.c
@@ -36,7 +36,7 @@ int VP8EncFinishAlpha(VP8Encoder* enc) {
     assert(pic->a);
     if (!EncodeAlpha(pic->a, pic->width, pic->height, pic->a_stride,
                      config->alpha_quality, config->alpha_compression,
-                     &tmp_data, &tmp_size)) {
+                     config->alpha_filtering, &tmp_data, &tmp_size)) {
       return 0;
     }
     if (tmp_size != (uint32_t)tmp_size) {  // Sanity check.

src/enc/config.c
@@ -43,6 +43,7 @@ int WebPConfigInitInternal(WebPConfig* const config,
   config->autofilter = 0;
   config->partition_limit = 0;
   config->alpha_compression = 1;
+  config->alpha_filtering = 0;
   config->alpha_quality = 100;
 
   // TODO(skal): tune.
@@ -111,6 +112,8 @@ int WebPValidateConfig(const WebPConfig* const config) {
   if (config->partition_limit < 0 || config->partition_limit > 100)
     return 0;
   if (config->alpha_compression < 0)
+    return 0;
+  if (config->alpha_filtering < 0)
     return 0;
   if (config->alpha_quality < 0 || config->alpha_quality > 100)
     return 0;

commit 9d0e17c9a9753ae0df1ea34a78031628ab38641a

fix msvc build breakage after 252028a
replace inline with WEBP_INLINE; inline is reserved for c++ code
w/visual studio

Change-Id: Ib06c6a12fd786701740d8b260d9aa310ee81fddf

src/utils/filters.c
@@ -164,11 +164,11 @@ static void GradientUnfilter(const uint8_t* data, int width, int height,
 //------------------------------------------------------------------------------
 // Paeth filter.
 
-static inline int AbsDiff(int a, int b) {
+static WEBP_INLINE int AbsDiff(int a, int b) {
   return (a > b) ? a - b : b - a;
 }
 
-static inline uint8_t PaethPredictor(uint8_t a, uint8_t b, uint8_t c) {
+static WEBP_INLINE uint8_t PaethPredictor(uint8_t a, uint8_t b, uint8_t c) {
   const int p = a + b - c;  // Base.
   const int pa = AbsDiff(p, a);
   const int pb = AbsDiff(p, b);