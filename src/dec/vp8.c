ed278e2279df7da0b64d6ecb1899f3055957706c
Scott LaVarnway <slavarnway@google.com>
2012-03-26 11:44:04 -0400

    Removed unnecessary lookup
    The lookup value is the same as the index.  The index in this
    case is either a 0 or 1.
    
    Change-Id: I0432dfcbea9e7bb20bca7574e25fb1fd8a51c8e9

--- src/dec/vp8.c
+++ src/dec/vp8.c
@@ -462,7 +462,7 @@ typedef const uint8_t (*ProbaArray)[NUM_CTX][NUM_PROBAS];  // for const-casting
 // (and 0 if there's no coeff at all)
 static int GetCoeffs(VP8BitReader* const br, ProbaArray prob,
                      int ctx, const quant_t dq, int n, int16_t* out) {
-  const uint8_t* p = prob[kBands[n]][ctx];
+  const uint8_t* p = prob[n][ctx];  // n is either 0 or 1 here. kBands[n] is not necessary.
   if (!VP8GetBit(br, p[0])) {   // first EOB is more a 'CBP' bit.
     return 0;
   }
