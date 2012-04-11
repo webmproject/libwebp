337914a036c7c9183377c8e190c01d72469bc71c
James Zern <jzern@google.com>
2012-04-10 17:23:38 -0700

    add utils/color_cache.[hc] from experimental
    Pulled from the current HEAD (218c32e).
    The history of this and related files is a bit entangled so rather
    trying to split the changes and introduce some noise in master's history
    we'll start with a fresh snapshot.
    The file progression is still available in the experimental branch.
    
    Change-Id: Id879be453a94d9f44ec8d47747823ca7297ae008

---
+++ src/utils/color_cache.c
@@ -0,0 +1,45 @@
+// Copyright 2012 Google Inc. All Rights Reserved.
+//
+// This code is licensed under the same terms as WebM:
+//  Software License Agreement:  http://www.webmproject.org/license/software/
+//  Additional IP Rights Grant:  http://www.webmproject.org/license/additional/
+// -----------------------------------------------------------------------------
+//
+// Color Cache for WebP Lossless
+//
+// Author: jyrki@google.com (Jyrki Alakuijala)
+
+#include <assert.h>
+#include <stdlib.h>
+#include "./color_cache.h"
+
+#if defined(__cplusplus) || defined(c_plusplus)
+extern "C" {
+#endif
+
+//------------------------------------------------------------------------------
+// VP8LColorCache.
+
+int VP8LColorCacheInit(VP8LColorCache* const cc, int hash_bits) {
+  int hash_size;
+  assert(cc != NULL);
+
+  if (hash_bits == 0) hash_bits = 1;
+  hash_size = 1 << hash_bits;
+  cc->colors_ = (uint32_t*)calloc(hash_size, sizeof(*cc->colors_));
+  if (cc->colors_ == NULL) return 0;
+  cc->hash_shift_ = 32 - hash_bits;
+  return 1;
+}
+
+void VP8LColorCacheDelete(VP8LColorCache* const cc) {
+  if (cc != NULL) {
+    free(cc->colors_);
+    free(cc);
+  }
+}
+
+#if defined(__cplusplus) || defined(c_plusplus)
+}
+#endif
+
