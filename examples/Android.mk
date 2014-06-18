d51467841e687b775f982622e858bd2abe192496
James Zern <jzern@google.com>
2014-06-17 23:37:00 -0700

    examples/Android.mk: add cwebp
    Change-Id: Id8edd3c17d82e4ab0087c315cd3ead1cb285e714

--- examples/Android.mk
+++ examples/Android.mk
@@ -14,6 +14,26 @@ include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 
+# Note: to enable jpeg/png encoding the sources from AOSP can be used with
+# minor modification to their Android.mk files.
+LOCAL_SRC_FILES := \
+    cwebp.c \
+    jpegdec.c \
+    metadata.c \
+    pngdec.c \
+    tiffdec.c \
+    webpdec.c \
+
+LOCAL_CFLAGS := $(WEBP_CFLAGS)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../src
+LOCAL_STATIC_LIBRARIES := example_util webp
+
+LOCAL_MODULE := cwebp
+
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+
 LOCAL_SRC_FILES := \
     dwebp.c \
 
