2a407092ab0a18628a4a789f4e06e2408a17c4cd
Pascal Massimino <pascal.massimino@gmail.com>
2015-02-18 09:40:30 +0100

    4-5% faster encoding using SSE2 for GetResidualCost
    new file: cost_sse2.c
    
    Change-Id: I4896c07f5ff2443ef743f4435fe2758d95a672ed

--- Android.mk
+++ Android.mk
@@ -41,6 +41,7 @@ LOCAL_SRC_FILES := \
     src/dsp/cost.c \
     src/dsp/cost_mips32.c \
     src/dsp/cost_mips_dsp_r2.c \
+    src/dsp/cost_sse2.c \
     src/dsp/cpu.c \
     src/dsp/dec.c \
     src/dsp/dec_clip_tables.c \
