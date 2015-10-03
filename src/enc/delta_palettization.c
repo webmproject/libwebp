f7b8f90740756cabe020d15f913ee0e71356d80f
James Zern <jzern@google.com>
2015-10-03 10:05:09 -0700

    delta_palettization.*: add copyright
    Change-Id: I5dc0ae0de88968d2c73b7025ce18319897219630

--- src/enc/delta_palettization.c
+++ src/enc/delta_palettization.c
@@ -1,3 +1,15 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the COPYING file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS. All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+// -----------------------------------------------------------------------------
+//
+// Author: Mislav Bradac (mislavm@google.com)
+//
+
 #ifdef WEBP_EXPERIMENTAL_FEATURES
 
 #include "./delta_palettization.h"
--- src/enc/delta_palettization.h
+++ src/enc/delta_palettization.h
@@ -1,3 +1,15 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+//
+// Use of this source code is governed by a BSD-style license
+// that can be found in the COPYING file in the root of the source
+// tree. An additional intellectual property rights grant can be found
+// in the file PATENTS. All contributing project authors may
+// be found in the AUTHORS file in the root of the source tree.
+// -----------------------------------------------------------------------------
+//
+// Author: Mislav Bradac (mislavm@google.com)
+//
+
 #ifndef WEBP_ENC_DELTA_PALETTIZATION_H_
 #define WEBP_ENC_DELTA_PALETTIZATION_H_
 
