6ec0d2a9465da6661885be29939341a28d82061c
Pascal Massimino <pascal.massimino@gmail.com>
2016-02-18 14:58:18 +0100

    clarify the logic of the error path when decoding fails.
    Change-Id: I2f86751ddafa4708dac3ffc9d6ec1f156e027a83

--- src/dec/webp.c
+++ src/dec/webp.c
@@ -512,10 +512,10 @@ static VP8StatusCode DecodeInto(const uint8_t* const data, size_t data_size,
 
   if (status != VP8_STATUS_OK) {
     WebPFreeDecBuffer(params->output);
-  }
-
+  } else {
   if (params->options != NULL && params->options->flip) {
     status = WebPFlipBuffer(params->output);
+  }
   }
   return status;
 }
