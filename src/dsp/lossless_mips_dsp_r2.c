e6cb8577792bf1e4855068bd024ab62737e9b7b4
Vincent Rabaud <vrabaud@google.com>
2016-09-12 17:23:15 +0200

    Split off common lossless dsp inline functions.
    Change-Id: I64f96897b11d1c21f033c7e47b21edccb5c68738

--- src/dsp/lossless_mips_dsp_r2.c
+++ src/dsp/lossless_mips_dsp_r2.c
@@ -17,6 +17,7 @@
 #if defined(WEBP_USE_MIPS_DSP_R2)
 
 #include "./lossless.h"
+#include "./lossless_common.h"
 
 #define MAP_COLOR_FUNCS(FUNC_NAME, TYPE, GET_INDEX, GET_VALUE)                 \
 static void FUNC_NAME(const TYPE* src,                                         \
