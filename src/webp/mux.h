23c3cc89828ad133d400d3a5c384a1aa7ef7eeb7
Jehan <jehan@girinstud.io>
2016-11-26 01:25:49 +0100

    Fix "all|no frames are keyframes" settings.
    Documentation says: "if kmin == 0, then key-frame insertion is disabled;
    and if kmax == 0, then all frames will be key-frames."
    Reading this, you'd expect that if kmax == 0, then with any kmin <= 0
    all frames will be key-frames. But actually the kmin <= 0 test is caught
    first and you get the opposite (no keyframes but the first). You'd have
    instead to set kmax == 0 and any value kmin > 0, which is absolutely
    counter-intuitive (reversing order).
    Moreover kmax == 1 has no valid kmin (kmin == 1 conflicts with the
    `kmax > kmin` rule and kmin == 0 conflicts with `kmin >= kmax / 2 + 1`).
    So it should be considered an exception too.
    
    Instead I propose this new logic:
    - kmax == 1 means that all frames are keyframes (you are explicitly
      requesting a keyframe every 1 frame at most, i.e. all frames).
    - kmax == 0 means no keyframes (you ask for a keyframe every 0 frames,
      i.e. never).
    This is more "logical" language-wise, and also does not involve any
    conflicts about what if both kmax and kmin are 0, since now a single
    property value is meaningful for the 2 exceptional cases.
    
    Change-Id: Ia90fb963bc26904ff078d2e4ef9f74b22b13a0fd

--- src/webp/mux.h
+++ src/webp/mux.h
@@ -430,9 +430,10 @@ struct WebPAnimEncoderOptions {
                         // frames in the output. The library may insert some key
                         // frames as needed to satisfy this criteria.
                         // Note that these conditions should hold: kmax > kmin
-                        // and kmin >= kmax / 2 + 1. Also, if kmin == 0, then
-                        // key-frame insertion is disabled; and if kmax == 0,
-                        // then all frames will be key-frames.
+                        // and kmin >= kmax / 2 + 1. Also, if kmax <= 0, then
+                        // key-frame insertion is disabled; and if kmax == 1,
+                        // then all frames will be key-frames (kmin value does
+                        // not matter for these special cases).
   int allow_mixed;      // If true, use mixed compression mode; may choose
                         // either lossy and lossless for each frame.
   int verbose;          // If true, print info and warning messages to stderr.
