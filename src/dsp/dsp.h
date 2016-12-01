e03b7205dcf82ab39784683ace58712f7afd0fbd
Vincent Rabaud <vrabaud@google.com>
2016-12-01 16:25:11 +0100

    Properly detect NEON on non-android platforms.
    __ARM_NEON is in ARM® C Language Extensions 2.1
    __arm__ as defined by GNU C
    (cf https://sourceforge.net/p/predef/wiki/Architectures/)
    
    Change-Id: Ida0dd98fbd626e3d39a5bb1d42bc815af2fa2fdd

--- src/dsp/dsp.h
+++ src/dsp/dsp.h
@@ -74,9 +74,9 @@ extern "C" {
 
 // The intrinsics currently cause compiler errors with arm-nacl-gcc and the
 // inline assembly would need to be modified for use with Native Client.
-#if (defined(__ARM_NEON__) || defined(WEBP_ANDROID_NEON) || \
-     defined(__aarch64__) || defined(WEBP_HAVE_NEON)) && \
-    !defined(__native_client__)
+#if (defined(__arm__) || defined(__ARM_NEON) || defined(__ARM_NEON__) || \
+     defined(WEBP_ANDROID_NEON) || defined(__aarch64__) ||               \
+     defined(WEBP_HAVE_NEON)) && !defined(__native_client__)
 #define WEBP_USE_NEON
 #endif
 
