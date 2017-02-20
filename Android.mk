05adb87460785fa28bc26f151760fe260038e533
Pascal Massimino <pascal.massimino@gmail.com>
2017-02-20 17:27:15 +0100

    move the SSIM calculation code in ssim.c / ssim_sse2.c
    Change-Id: I63a63fa7f44f257f2e17e45358b206c23069c448

--- Android.mk
+++ Android.mk
@@ -101,6 +101,8 @@ dsp_enc_srcs := \
     src/dsp/lossless_enc_neon.$(NEON) \
     src/dsp/lossless_enc_sse2.c \
     src/dsp/lossless_enc_sse41.c \
+    src/dsp/ssim.c \
+    src/dsp/ssim_sse2.c \
 
 enc_srcs := \
     src/enc/alpha_enc.c \
