5cb0471d8a7106e8e56eeac974aec1b0a333c98f
Vincent Rabaud <vrabaud@google.com>
2017-02-22 15:57:06 +0100

    Install binaries, libraries and headers in CMake.
    Change-Id: Iaf32c716e0252f9a0e9f5d663f02b8fad98096e2

---
+++ cmake/version.c
@@ -0,0 +1,11 @@
+// Executable used by CMake to return the version of the library as
+// MAJOR.MINOR.PATCH
+#include "webp/decode.h"
+
+#include "stdio.h"
+
+int main(int argc, char* argv) {
+  const int version = WebPGetDecoderVersion();
+  printf("%d.%d.%d", version >> 16, (version >> 8) & 0xff, version & 0xff);
+  return 0;
+}
