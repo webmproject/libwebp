4e52c0a5b7d230b3ca69e0e7bf016be552f53b2e
Vincent Rabaud <vrabaud@google.com>
2017-05-02 18:34:02 +0200

    Fix bad bit writer initialization.
    When re-initializing a bit writer, we could set invalid values because
    the bit writer was not big enough.
    
    Change-Id: Id25ab6712603245a5a12d5f4a86fe35a9a799a5d

--- src/utils/bit_writer_utils.c
+++ src/utils/bit_writer_utils.c
@@ -239,6 +239,18 @@ int VP8LBitWriterInit(VP8LBitWriter* const bw, size_t expected_size) {
   return VP8LBitWriterResize(bw, expected_size);
 }
 
+int VP8LBitWriterInitFromBitWriter(const VP8LBitWriter* const src,
+                                   VP8LBitWriter* const dst) {
+  const int size = src->cur_ - src->buf_ + 1;
+  memset(dst, 0, sizeof(*dst));
+  if (!VP8LBitWriterResize(dst, size)) return 0;
+  memcpy(dst->buf_, src->buf_, size);
+  dst->bits_ = src->bits_;
+  dst->used_ = src->used_;
+  dst->error_ = src->error_;
+  return 1;
+}
+
 void VP8LBitWriterWipeOut(VP8LBitWriter* const bw) {
   if (bw != NULL) {
     WebPSafeFree(bw->buf_);
@@ -251,6 +263,7 @@ void VP8LBitWriterReset(const VP8LBitWriter* const bw_init,
   bw->bits_ = bw_init->bits_;
   bw->used_ = bw_init->used_;
   bw->cur_ = bw->buf_ + (bw_init->cur_ - bw_init->buf_);
+  assert(bw->cur_ < bw->end_);
   bw->error_ = bw_init->error_;
 }
 
