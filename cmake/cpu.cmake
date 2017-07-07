6427e5fde4e6dd3b7f129f51e1bd5894dd1b9e4f
Vincent Rabaud <vrabaud@google.com>
2017-07-07 10:33:48 +0200

    Cleam-up some CMake
    Some image libraries define _INCLUDE_DIR and not INCLUDE_DIRS.
    CMakePushCheckState is a non-destructive way of changing the
    CMAKE_REQUIRED_* variables.
    
    Change-Id: I7d318a0ddf9754a5c047f47ba1713f9f94e1ec1c

--- cmake/cpu.cmake
+++ cmake/cpu.cmake
@@ -54,12 +54,14 @@ endif()
 list(LENGTH WEBP_SIMD_FLAGS WEBP_SIMD_FLAGS_LENGTH)
 math(EXPR WEBP_SIMD_FLAGS_RANGE "${WEBP_SIMD_FLAGS_LENGTH} - 1")
 
+include(CMakePushCheckState)
 foreach(I_SIMD RANGE ${WEBP_SIMD_FLAGS_RANGE})
   list(GET WEBP_SIMD_FLAGS ${I_SIMD} WEBP_SIMD_FLAG)
 
   # First try with no extra flag added as the compiler might have default flags
   # (especially on Android).
   unset(WEBP_HAVE_${WEBP_SIMD_FLAG} CACHE)
+  cmake_push_check_state()
   set(CMAKE_REQUIRED_FLAGS)
   webp_check_compiler_flag(${WEBP_SIMD_FLAG} ${WEBP_ENABLE_SIMD})
   if(NOT WEBP_HAVE_${WEBP_SIMD_FLAG})
@@ -69,6 +71,7 @@ foreach(I_SIMD RANGE ${WEBP_SIMD_FLAGS_RANGE})
   else()
     set(SIMD_COMPILE_FLAG " ")
   endif()
+  cmake_pop_check_state()
   # Check which files we should include or not.
   list(GET WEBP_SIMD_FILE_EXTENSIONS ${I_SIMD} WEBP_SIMD_FILE_EXTENSION)
   file(GLOB SIMD_FILES "${CMAKE_CURRENT_LIST_DIR}/../"
@@ -100,12 +103,14 @@ foreach(I_SIMD RANGE ${WEBP_SIMD_FLAGS_RANGE})
           else()
             set(COMMON_PATTERNS)
           endif()
+          cmake_push_check_state()
           set(CMAKE_REQUIRED_DEFINITIONS ${SIMD_COMPILE_FLAG})
           check_c_source_compiles("int main(void) {return 0;}"
             FLAG_${SIMD_COMPILE_FLAG}
             FAIL_REGEX "warning: argument unused during compilation:"
             ${COMMON_PATTERNS}
           )
+          cmake_pop_check_state()
           if(NOT FLAG_${SIMD_COMPILE_FLAG})
             unset(HAS_COMPILE_FLAG CACHE)
           endif()
