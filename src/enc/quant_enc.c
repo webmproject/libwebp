f435de95754c8884db6859c18ae905e6623c9cb2
Johann <johann.koenig@duck.com>
2018-11-16 11:22:41 -0500

    IsFlat: return int
    IsFlat is a boolean function. Don't use a specialized return type.
    
    Change-Id: I070395082023ceb50251c44f5f4253b90394710c

--- src/enc/quant_enc.c
+++ src/enc/quant_enc.c
@@ -977,7 +977,7 @@ static void SwapOut(VP8EncIterator* const it) {
   SwapPtr(&it->yuv_out_, &it->yuv_out2_);
 }
 
-static score_t IsFlat(const int16_t* levels, int num_blocks, score_t thresh) {
+static int IsFlat(const int16_t* levels, int num_blocks, score_t thresh) {
   score_t score = 0;
   while (num_blocks-- > 0) {      // TODO(skal): refine positional scoring?
     int i;
