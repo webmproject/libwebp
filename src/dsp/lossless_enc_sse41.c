c2101ccba053de351e83e3de05d746d1ea412422
Ilya Kurdyukov <jpegqs@gmail.com>
2021-02-16 14:30:26 +0700

    10% faster lossless encoding
    This assumes the ctz instruction is present and fast, which is
    pretty much guaranteed for any recent (<10 years!) CPU.
    
    Introduces the BitCtz() function.
    
    Change-Id: If555c5530238ca0342b8d97b0d708b1bdc888d3f

--- src/dsp/lossless_enc_sse41.c
+++ src/dsp/lossless_enc_sse41.c
@@ -44,42 +44,41 @@ static void SubtractGreenFromBlueAndRed_SSE41(uint32_t* argb_data,
 //------------------------------------------------------------------------------
 // Color Transform
 
+#define MK_CST_16(HI, LO) \
+  _mm_set1_epi32((int)(((uint32_t)(HI) << 16) | ((LO) & 0xffff)))
+
 #define SPAN 8
 static void CollectColorBlueTransforms_SSE41(const uint32_t* argb, int stride,
                                              int tile_width, int tile_height,
                                              int green_to_blue, int red_to_blue,
                                              int histo[]) {
-  const __m128i mults_r = _mm_set1_epi16(CST_5b(red_to_blue));
-  const __m128i mults_g = _mm_set1_epi16(CST_5b(green_to_blue));
-  const __m128i mask_g = _mm_set1_epi16((short)0xff00);   // green mask
-  const __m128i mask_gb = _mm_set1_epi32(0xffff);         // green/blue mask
-  const __m128i mask_b = _mm_set1_epi16(0x00ff);          // blue mask
-  const __m128i shuffler_lo = _mm_setr_epi8(-1, 2, -1, 6, -1, 10, -1, 14, -1,
-                                            -1, -1, -1, -1, -1, -1, -1);
-  const __m128i shuffler_hi = _mm_setr_epi8(-1, -1, -1, -1, -1, -1, -1, -1, -1,
-                                            2, -1, 6, -1, 10, -1, 14);
+  const __m128i mult = MK_CST_16(CST_5b(red_to_blue), CST_5b(green_to_blue));
+  const __m128i mask_b = _mm_set1_epi32(0x000000ff);
+  const __m128i perm = _mm_setr_epi8(-1, 1, -1, 2, -1, 5, -1, 6,
+                                     -1, 9, -1, 10, -1, 13, -1, 14);
   int y;
   for (y = 0; y < tile_height; ++y) {
     const uint32_t* const src = argb + y * stride;
-    int i, x;
+    int x;
     for (x = 0; x + SPAN <= tile_width; x += SPAN) {
-      uint16_t values[SPAN];
-      const __m128i in0 = _mm_loadu_si128((__m128i*)&src[x + 0]);
-      const __m128i in1 = _mm_loadu_si128((__m128i*)&src[x + SPAN / 2]);
-      const __m128i r0 = _mm_shuffle_epi8(in0, shuffler_lo);
-      const __m128i r1 = _mm_shuffle_epi8(in1, shuffler_hi);
-      const __m128i r = _mm_or_si128(r0, r1);         // r 0
-      const __m128i gb0 = _mm_and_si128(in0, mask_gb);
-      const __m128i gb1 = _mm_and_si128(in1, mask_gb);
-      const __m128i gb = _mm_packus_epi32(gb0, gb1);  // g b
-      const __m128i g = _mm_and_si128(gb, mask_g);    // g 0
-      const __m128i A = _mm_mulhi_epi16(r, mults_r);  // x dbr
-      const __m128i B = _mm_mulhi_epi16(g, mults_g);  // x dbg
-      const __m128i C = _mm_sub_epi8(gb, B);          // x b'
-      const __m128i D = _mm_sub_epi8(C, A);           // x b''
-      const __m128i E = _mm_and_si128(D, mask_b);     // 0 b''
-      _mm_storeu_si128((__m128i*)values, E);
-      for (i = 0; i < SPAN; ++i) ++histo[values[i]];
+      const __m128i A1 = _mm_loadu_si128((const __m128i*)&src[x + 0]);
+      const __m128i A2 = _mm_loadu_si128((const __m128i*)&src[x + 4]);
+      const __m128i X1 = _mm_and_si128(A1, mask_b);
+      const __m128i X2 = _mm_and_si128(A2, mask_b);
+      const __m128i B1 = _mm_shuffle_epi8(A1, perm);
+      const __m128i B2 = _mm_shuffle_epi8(A2, perm);
+      const __m128i Y = _mm_packs_epi32(X1, X2);
+      const __m128i C1 = _mm_mulhi_epi16(B1, mult);
+      const __m128i C2 = _mm_mulhi_epi16(B2, mult);
+      const __m128i D = _mm_sub_epi16(Y, _mm_hadd_epi16(C1, C2));
+      ++histo[_mm_extract_epi8(D, 0)];
+      ++histo[_mm_extract_epi8(D, 2)];
+      ++histo[_mm_extract_epi8(D, 4)];
+      ++histo[_mm_extract_epi8(D, 6)];
+      ++histo[_mm_extract_epi8(D, 8)];
+      ++histo[_mm_extract_epi8(D, 10)];
+      ++histo[_mm_extract_epi8(D, 12)];
+      ++histo[_mm_extract_epi8(D, 14)];
     }
   }
   {
@@ -95,18 +94,16 @@ static void CollectColorBlueTransforms_SSE41(const uint32_t* argb, int stride,
 static void CollectColorRedTransforms_SSE41(const uint32_t* argb, int stride,
                                             int tile_width, int tile_height,
                                             int green_to_red, int histo[]) {
-  const __m128i mults_g = _mm_set1_epi16(CST_5b(green_to_red));
-  const __m128i mask_g = _mm_set1_epi32(0x00ff00);  // green mask
-  const __m128i mask = _mm_set1_epi16(0xff);
 
+  const __m128i mults_g = _mm_set1_epi16(CST_5b(green_to_red));
+  const __m128i mask_g = _mm_set1_epi32(0x0000ff00);  // green mask
   int y;
   for (y = 0; y < tile_height; ++y) {
     const uint32_t* const src = argb + y * stride;
-    int i, x;
+    int x;
     for (x = 0; x + SPAN <= tile_width; x += SPAN) {
-      uint16_t values[SPAN];
-      const __m128i in0 = _mm_loadu_si128((__m128i*)&src[x + 0]);
-      const __m128i in1 = _mm_loadu_si128((__m128i*)&src[x + SPAN / 2]);
+      const __m128i in0 = _mm_loadu_si128((const __m128i*)&src[x + 0]);
+      const __m128i in1 = _mm_loadu_si128((const __m128i*)&src[x + 4]);
       const __m128i g0 = _mm_and_si128(in0, mask_g);  // 0 0  | g 0
       const __m128i g1 = _mm_and_si128(in1, mask_g);
       const __m128i g = _mm_packus_epi32(g0, g1);     // g 0
@@ -114,10 +111,15 @@ static void CollectColorRedTransforms_SSE41(const uint32_t* argb, int stride,
       const __m128i A1 = _mm_srli_epi32(in1, 16);
       const __m128i A = _mm_packus_epi32(A0, A1);     // x r
       const __m128i B = _mm_mulhi_epi16(g, mults_g);  // x dr
-      const __m128i C = _mm_sub_epi8(A, B);           // x r'
-      const __m128i D = _mm_and_si128(C, mask);       // 0 r'
-      _mm_storeu_si128((__m128i*)values, D);
-      for (i = 0; i < SPAN; ++i) ++histo[values[i]];
+      const __m128i D = _mm_sub_epi8(A, B);           // x r'
+      ++histo[_mm_extract_epi8(D, 0)];
+      ++histo[_mm_extract_epi8(D, 2)];
+      ++histo[_mm_extract_epi8(D, 4)];
+      ++histo[_mm_extract_epi8(D, 6)];
+      ++histo[_mm_extract_epi8(D, 8)];
+      ++histo[_mm_extract_epi8(D, 10)];
+      ++histo[_mm_extract_epi8(D, 12)];
+      ++histo[_mm_extract_epi8(D, 14)];
     }
   }
   {
