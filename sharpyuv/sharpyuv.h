639619ce725da4ee1124863634c32fb89ce69081
James Zern <jzern@google.com>
2022-08-11 19:33:37 -0700

    cmake: fix dll exports
    move WEBP_EXTERN definition from Makefile.vc to webp/types.h using the
    existing WEBP_DLL define to control its declaration to
    __declspec(dllexport); the same is done for SHARPYUV_EXTERN
    
    Change-Id: Iecea360305bdd19a878255470a00b9dcc344c730

--- sharpyuv/sharpyuv.h
+++ sharpyuv/sharpyuv.h
@@ -27,7 +27,11 @@ extern "C" {
 #if defined(__GNUC__) && __GNUC__ >= 4
 #define SHARPYUV_EXTERN extern __attribute__((visibility("default")))
 #else
+#if defined(_MSC_VER) && defined(WEBP_DLL)
+#define SHARPYUV_EXTERN __declspec(dllexport)
+#else
 #define SHARPYUV_EXTERN extern
+#endif /* _MSC_VER && WEBP_DLL */
 #endif /* __GNUC__ >= 4 */
 #endif /* WEBP_EXTERN */
 #endif /* SHARPYUV_EXTERN */
